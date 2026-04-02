dac3a508ebc92a3171d922b2e1f6c76d0c01d9de
DevanshRathod12 <rathoddevansh53@gmail.com>
2026-04-02 23:10:09 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Route from "./src/Navigation/Routes"
+import {AuthProvider} from "./src/Context/AppContext"
 const App = () => {
   return (
+    <>
+    <AuthProvider>
    <Route/>
+    </AuthProvider>
+    </>
   )
 }
 
